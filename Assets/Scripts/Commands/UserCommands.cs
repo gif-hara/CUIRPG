2b84de840f446df9511ced9ecf03d1a8ba4de532
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2021-07-30 21:05:26 +0900

    とりあえずUserCommands追加

---
+++ Assets/Scripts/Commands/UserCommands.cs
@@ -0,0 +1,25 @@
+using System;
+using UniRx;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.CUIRPG.Commands
+{
+    /// <summary>
+    /// ユーザーに関する<see cref="ICommand"/>
+    /// </summary>
+    public sealed class UserCommands : ICommand
+    {
+        public string Name => "user";
+
+        public void SendHelp(IInteractor interactor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IObservable<Unit> InvokeAsObservable(CommandData data, IInteractor interactor)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
