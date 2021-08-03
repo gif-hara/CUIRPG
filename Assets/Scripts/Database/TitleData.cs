8e04bcad5a08f088e9140b08dba51f92306dc35e
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2021-08-03 22:09:28 +0900

    TitleField追加

--- Assets/Scripts/Database/TitleData.cs
+++ Assets/Scripts/Database/TitleData.cs
@@ -26,11 +26,16 @@ namespace HK.CUIRPG.Database
 
         public IReadOnlyList<TitleItem> Items => m_Items;
 
+        public IReadOnlyList<TitleField> Fields => m_Fields;
+
         private List<TitleItem> m_Items = new List<TitleItem>();
+
+        private List<TitleField> m_Fields = new List<TitleField>();
 
         public void Setup(Dictionary<string, string> data)
         {
             m_Items = PlayFabSimpleJson.DeserializeObject<List<TitleItem>>(data["Items"]);
+            m_Fields = PlayFabSimpleJson.DeserializeObject<List<TitleField>>(data["Fields"]);
         }
     }
 }
---
+++ Assets/Scripts/Database/TitleField.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.CUIRPG.Database
+{
+    /// <summary>
+    /// フィールドデータ
+    /// </summary>
+    public sealed class TitleField
+    {
+        public int id;
+
+        public string name;
+    }
+}
