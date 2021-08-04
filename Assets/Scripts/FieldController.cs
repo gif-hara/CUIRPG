30a9913da75534f238f67922db7336e0af5e519c
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2021-08-04 22:33:35 +0900

    FieldController追加

---
+++ Assets/Scripts/FieldController.cs
@@ -0,0 +1,21 @@
+using HK.CUIRPG.Database;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HK.CUIRPG
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed class FieldController
+    {
+        private string m_CurrentPath;
+
+        private TitleField m_CurrentTitleField;
+
+        public FieldController(TitleField initialTitleField)
+        {
+
+        }
+    }
+}
